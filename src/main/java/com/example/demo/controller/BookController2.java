e915cc8eea73b0fdbb7d5fec9a215815fcd2039a
mjzmu <emeail@emaoi.pl>
2020-04-26 12:48:39 +0200

    Commit msg

--- src/main/java/com/example/demo/controller/BookController2.java
+++ src/main/java/com/example/demo/controller/BookController2.java
@@ -3,27 +3,23 @@ package com.example.demo.controller;
 import com.example.demo.BookService;
 import com.example.demo.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class BookController {
+public class BookController2 {
 
     @Autowired
     BookService bookService;
-
     @GetMapping("list")
+
     List<Book> list() {
         try {
             Thread.sleep(300);
-            //let's just add some comment here
         } catch (InterruptedException e) {
             e.printStackTrace();
-            //And maybe a bit of logging here
-            System.out.println("Logging an exception: " + e);
         }
         return bookService.list();
     }
